8a5eb38e46c5bd8e2b0922853e82e2512a32def8
Daeun Lee <dev.leedaeun@gmail.com>
2026-07-06 13:11:05 +0900

    [feat]set-font (#1)

--- app/src/main/java/com/example/pauze/ui/theme/Type.kt
+++ app/src/main/java/com/example/pauze/ui/theme/Type.kt
@@ -1,34 +1,293 @@
 package com.example.pauze.ui.theme
 
+import com.example.pauze.R
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+val fontFamily = FontFamily(
+    Font(
+        R.font.spoqa_han_sans_neo_bold,
+        FontWeight.Bold
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    Font(
+        R.font.spoqa_han_sans_neo_medium,
+        FontWeight.Medium
+    ),
+    Font(
+        R.font.spoqa_han_sans_neo_regular,
+        FontWeight.Normal
+    ),
+    Font(
+        R.font.spoqa_han_sans_neo_light,
+        FontWeight.Light
+    ),
+)
+
+// Heading Large
+val headingLgBold = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = FontWeight.Bold,
+    fontSize = 28.sp,
+    letterSpacing = (-0.02).sp,
+    lineHeight = 40.sp,
+)
+
+val headingLgMedium = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = FontWeight.Medium,
+    fontSize = 28.sp,
+    letterSpacing = (-0.02).sp,
+    lineHeight = 40.sp,
+)
+
+val headingLgRegular = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = FontWeight.Normal,
+    fontSize = 28.sp,
+    letterSpacing = (-0.02).sp,
+    lineHeight = 40.sp,
+)
+
+val headingLgLight = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = FontWeight.Light,
+    fontSize = 28.sp,
+    letterSpacing = (-0.02).sp,
+    lineHeight = 40.sp,
+)
+
+// Heading Medium
+val headingMdBold = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = FontWeight.Bold,
+    fontSize = 24.sp,
+    letterSpacing = (-0.02).sp,
+    lineHeight = 34.sp,
+)
+
+val headingMdMedium = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = FontWeight.Medium,
+    fontSize = 24.sp,
+    letterSpacing = (-0.02).sp,
+    lineHeight = 34.sp,
+)
+
+val headingMdRegular = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = FontWeight.Normal,
+    fontSize = 24.sp,
+    letterSpacing = (-0.02).sp,
+    lineHeight = 34.sp,
+)
+
+val headingMdLight = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = FontWeight.Light,
+    fontSize = 24.sp,
+    letterSpacing = (-0.02).sp,
+    lineHeight = 34.sp,
+)
+
+// Heading Small
+val headingSmBold = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = FontWeight.Bold,
+    fontSize = 20.sp,
+    letterSpacing = (-0.02).sp,
+    lineHeight = 28.sp,
+)
+
+val headingSmMedium = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = FontWeight.Medium,
+    fontSize = 20.sp,
+    letterSpacing = (-0.02).sp,
+    lineHeight = 28.sp,
+)
+
+val headingSmRegular = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = FontWeight.Normal,
+    fontSize = 20.sp,
+    letterSpacing = (-0.02).sp,
+    lineHeight = 28.sp,
+)
+
+val headingSmLight = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = FontWeight.Light,
+    fontSize = 20.sp,
+    letterSpacing = (-0.02).sp,
+    lineHeight = 28.sp,
+)
+
+// Body Text XL
+val bodyTextXlBold = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = FontWeight.Bold,
+    fontSize = 18.sp,
+    letterSpacing = (-0.02).sp,
+    lineHeight = 28.sp,
+)
+
+val bodyTextXlMedium = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = FontWeight.Medium,
+    fontSize = 18.sp,
+    letterSpacing = (-0.02).sp,
+    lineHeight = 28.sp,
+)
+
+val bodyTextXlRegular = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = FontWeight.Normal,
+    fontSize = 18.sp,
+    letterSpacing = (-0.02).sp,
+    lineHeight = 28.sp,
+)
+
+val bodyTextXlLight = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = FontWeight.Light,
+    fontSize = 18.sp,
+    letterSpacing = (-0.02).sp,
+    lineHeight = 28.sp,
+)
+
+// Body Text Large
+val bodyTextLgBold = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = FontWeight.Bold,
+    fontSize = 16.sp,
+    letterSpacing = (-0.02).sp,
+    lineHeight = 26.sp,
+)
+
+val bodyTextLgMedium = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = FontWeight.Medium,
+    fontSize = 16.sp,
+    letterSpacing = (-0.02).sp,
+    lineHeight = 26.sp,
+)
+
+val bodyTextLgRegular = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = FontWeight.Normal,
+    fontSize = 16.sp,
+    letterSpacing = (-0.02).sp,
+    lineHeight = 26.sp,
+)
+
+val bodyTextLgLight = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = FontWeight.Light,
+    fontSize = 16.sp,
+    letterSpacing = (-0.02).sp,
+    lineHeight = 26.sp,
+)
+
+// Body Text Medium
+val bodyTextMdBold = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = FontWeight.Bold,
+    fontSize = 14.sp,
+    letterSpacing = (-0.02).sp,
+    lineHeight = 22.sp,
+)
+
+val bodyTextMdMedium = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = FontWeight.Medium,
+    fontSize = 14.sp,
+    letterSpacing = (-0.02).sp,
+    lineHeight = 22.sp,
+)
+
+val bodyTextMdRegular = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = FontWeight.Normal,
+    fontSize = 14.sp,
+    letterSpacing = (-0.02).sp,
+    lineHeight = 22.sp,
+)
+
+val bodyTextMdLight = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = FontWeight.Light,
+    fontSize = 14.sp,
+    letterSpacing = (-0.02).sp,
+    lineHeight = 22.sp,
+)
+
+// Body Text Small
+val bodyTextSmBold = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = FontWeight.Bold,
+    fontSize = 12.sp,
+    letterSpacing = (-0.02).sp,
+    lineHeight = 18.sp,
+)
+
+val bodyTextSmMedium = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = FontWeight.Medium,
+    fontSize = 12.sp,
+    letterSpacing = (-0.02).sp,
+    lineHeight = 18.sp,
+)
+
+val bodyTextSmRegular = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = FontWeight.Normal,
+    fontSize = 12.sp,
+    letterSpacing = (-0.02).sp,
+    lineHeight = 18.sp,
+)
+
+val bodyTextSmLight = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = FontWeight.Light,
+    fontSize = 12.sp,
+    letterSpacing = (-0.02).sp,
+    lineHeight = 18.sp,
+)
+
+// Caption
+val captionTextBold = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = FontWeight.Bold,
+    fontSize = 11.sp,
+    letterSpacing = (-0.02).sp,
+    lineHeight = 16.sp,
+)
+
+val captionTextMedium = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = FontWeight.Medium,
+    fontSize = 11.sp,
+    letterSpacing = (-0.02).sp,
+    lineHeight = 16.sp,
+)
+
+val captionTextRegular = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = FontWeight.Normal,
+    fontSize = 11.sp,
+    letterSpacing = (-0.02).sp,
+    lineHeight = 16.sp,
+)
+
+val captionTextLight = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = FontWeight.Light,
+    fontSize = 11.sp,
+    letterSpacing = (-0.02).sp,
+    lineHeight = 16.sp,
 )
