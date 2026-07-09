70c5f0cbad130c9cab586e26b1c32e9caf60b5c9
Daeun Lee <dev.leedaeun@gmail.com>
2026-07-09 23:59:59 +0900

    [fix]이메일 imeAction Next로 변경

--- app/src/main/java/com/example/pauze/ui/login/LoginScreen.kt
+++ app/src/main/java/com/example/pauze/ui/login/LoginScreen.kt
@@ -35,6 +35,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
@@ -125,10 +126,10 @@ fun LoginLayout(){
                 modifier = Modifier.fillMaxWidth(),
                 keyboardOptions = KeyboardOptions(
                     keyboardType = KeyboardType.Email,
-                    imeAction = ImeAction.Done
+                    imeAction = ImeAction.Next
                 ),
                 keyboardActions = KeyboardActions(
-                    onDone = { focusManager.clearFocus() }
+                    onNext = { focusManager.moveFocus(focusDirection = FocusDirection.Next) }
                 ),
                 decorationBox = { innerTextField ->
                     if(email.isEmpty()){
