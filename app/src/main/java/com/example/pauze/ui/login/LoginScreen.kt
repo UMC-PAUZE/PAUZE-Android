0fcff3d55c312638a94548ad2afa4d441f304d77
Daeun Lee <dev.leedaeun@gmail.com>
2026-07-09 19:10:58 +0900

    [fix]입력창 관련 디테일 수정

--- app/src/main/java/com/example/pauze/ui/login/LoginScreen.kt
+++ app/src/main/java/com/example/pauze/ui/login/LoginScreen.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -24,10 +23,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.AppBarRow
-import androidx.compose.material3.BasicAlertDialog
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -40,9 +35,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
@@ -85,7 +78,8 @@ fun LoginLayout(){
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var isPwdVisible by remember { mutableStateOf(false) }
-    var isFieldFocused by remember { mutableStateOf(false) }
+    var isEmailFocused by remember { mutableStateOf(false) }
+    var isPwdFocused by remember { mutableStateOf(false) }
     var showDialog by rememberSaveable { mutableStateOf(false) }
 
     Column(
@@ -111,11 +105,11 @@ fun LoginLayout(){
             modifier = Modifier
                 .fillMaxWidth().border(
                     width = 1.dp,
-                    color = if (isFieldFocused) AppTheme.palette.gray.getColor(3)
+                    color = if (isEmailFocused) AppTheme.palette.gray.getColor(3)
                             else AppTheme.palette.gray.getColor(6),
                     shape = RoundedCornerShape(size = 16.dp)
                 ).onFocusChanged {
-                    isFieldFocused = it.isFocused
+                    isEmailFocused = it.isFocused
                 }.padding(
                     horizontal = 16.dp, vertical = 14.dp
                 )
@@ -167,11 +161,11 @@ fun LoginLayout(){
             modifier = Modifier
                 .fillMaxWidth().border(
                     width = 1.dp,
-                    color = if (isFieldFocused) AppTheme.palette.gray.getColor(3)
+                    color = if (isPwdFocused) AppTheme.palette.gray.getColor(3)
                             else AppTheme.palette.gray.getColor(6),
                     shape = RoundedCornerShape(size = 16.dp)
                 ).onFocusChanged {
-                    isFieldFocused = it.isFocused
+                    isPwdFocused = it.isFocused
                 }.padding(
                     horizontal = 16.dp, vertical = 14.dp
                 )
@@ -213,9 +207,9 @@ fun LoginLayout(){
                             IconButton(onClick = {
                                 isPwdVisible = !isPwdVisible
                             }) {
-                                // todo: 추후 비밀번호 표시 아이콘 나오면 수정
                                 Image(
-                                    painter = painterResource(R.drawable.pwd_eye),
+                                    painter = if(isPwdVisible) painterResource(R.drawable.pwd_eye_on)
+                                        else painterResource(R.drawable.pwd_eye_off),
                                     contentDescription = "cancel"
                                 )
                             }
