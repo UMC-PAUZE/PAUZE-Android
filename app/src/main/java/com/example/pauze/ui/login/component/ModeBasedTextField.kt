f80cf9c06fe5e65137479304c2e1a85fb722bc11
Daeun Lee <dev.leedaeun@gmail.com>
2026-07-19 18:05:03 +0900

    [refactor]공통 버튼 및 텍스트 필드 적용 (#38)

--- app/src/main/java/com/example/pauze/ui/login/component/ModeBasedTextField.kt
+++ app/src/main/java/com/example/pauze/ui/login/component/ModeBasedTextField.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -18,21 +17,23 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import com.example.pauze.R
 import com.example.pauze.ui.theme.AppTheme
 import com.example.pauze.ui.theme.bodyTextLgMedium
 import com.example.pauze.ui.theme.bodyTextMdRegular
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.focus.FocusManager
 
 enum class TextFieldMode { Email, SetEmail, Pwd, SetPwd, UserName, }
 enum class Actions { Reset, Pwd, Check }
@@ -42,12 +43,16 @@ fun ModeBasedTextField(
     mode: TextFieldMode,
     value: String,
     onValueChanged: (String) -> Unit,
-    isVisible: Boolean = false,
-    onVisibilityChanged: (Boolean) -> Unit,
-    onCheckClick: () -> Unit,
-    isFocused: Boolean = false,
-    onFocusChanged: () -> Unit,
+    onCheckClick: () -> Unit = {},
 ) {
+    val focusManager = LocalFocusManager.current
+    var isFocused by remember { mutableStateOf(false) }
+    var isVisible by remember { mutableStateOf(when(mode){
+        TextFieldMode.Pwd -> false
+        TextFieldMode.SetPwd -> false
+        else -> true
+    }) }
+
     Column(
         modifier = Modifier
             .fillMaxWidth().border(
@@ -56,62 +61,52 @@ fun ModeBasedTextField(
                 else AppTheme.palette.gray.getColor(6),
                 shape = RoundedCornerShape(size = 16.dp)
             ).onFocusChanged {
-                onFocusChanged
+                isFocused = it.isFocused
             }.padding(
                 horizontal = 16.dp, vertical = 14.dp
             )
     ){
         Text(
             when(mode){
+                TextFieldMode.UserName -> "이름"
                 TextFieldMode.Email -> "이메일"
                 TextFieldMode.SetEmail -> "이메일"
-                TextFieldMode.Pwd -> "비밀번호"
-                TextFieldMode.SetPwd -> "비밀번호"
-                TextFieldMode.UserName -> "이름"
+                else -> "비밀번호"
             },
             color = AppTheme.palette.gray.getColor(5),
             style = bodyTextMdRegular,
         )
         BasicTextField(
             value = value,
-            onValueChange = { onValueChanged(value) },
+            onValueChange = { onValueChanged(it) },
             modifier = Modifier.fillMaxWidth(),
-            keyboardOptions = when(mode){
-                TextFieldMode.Email -> KeyboardOptions(
-                    keyboardType = KeyboardType.Email,
-                    imeAction = ImeAction.Next
-                )
-                TextFieldMode.SetEmail -> KeyboardOptions(
-                    keyboardType = KeyboardType.Email,
-                    imeAction = ImeAction.Next
-                )
-                TextFieldMode.Pwd -> KeyboardOptions(
-                    keyboardType = KeyboardType.Password,
-                    imeAction = ImeAction.Done
-                )
-                TextFieldMode.SetPwd -> KeyboardOptions(
-                    keyboardType = KeyboardType.Password,
-                    imeAction = ImeAction.Done
-                )
+            visualTransformation = if (isVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            keyboardOptions = when (mode) {
                 TextFieldMode.UserName -> KeyboardOptions(
                     keyboardType = KeyboardType.Text,
                     imeAction = ImeAction.Next
                 )
+                TextFieldMode.Pwd, TextFieldMode.SetPwd -> KeyboardOptions(
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Done
+                )
+                else -> KeyboardOptions(
+                    keyboardType = KeyboardType.Email,
+                    imeAction = ImeAction.Next
+                )
             },
             keyboardActions = KeyboardActions(
-                onNext = { onFocusChanged },
-                onDone = { onFocusChanged }
+                onNext = { focusManager.moveFocus(focusDirection = FocusDirection.Next) },
+                onDone = { focusManager.clearFocus() }
             ),
-
             decorationBox = { innerTextField ->
                 if(value.isEmpty()){
                     Text(
                         when(mode){
-                            TextFieldMode.Email -> "텍스트"
                             TextFieldMode.SetEmail -> "example@gmail.com"
-                            TextFieldMode.Pwd -> "텍스트"
                             TextFieldMode.SetPwd -> "비밀번호를 입력해주세요"
                             TextFieldMode.UserName -> "실명을 입력해주세요"
+                            else -> "텍스트"
                         },
                         color = AppTheme.palette.gray.getColor(5),
                         style = bodyTextLgMedium,
@@ -124,19 +119,17 @@ fun ModeBasedTextField(
                         Row(modifier = Modifier.weight(1f)){
                             innerTextField()
                         }
-                        // mode에 따라 수정
                         when(mode){
-                            TextFieldMode.Email -> ActionButton(actions = Actions.Reset, ) { onValueChanged("") }
                             TextFieldMode.SetEmail -> Row {
                                 ActionButton(actions = Actions.Reset) { onValueChanged("") }
                                 ActionButton(actions = Actions.Check) { onCheckClick }
                             }
-                            TextFieldMode.Pwd -> ActionButton(actions = Actions.Pwd) { onVisibilityChanged(!isVisible) }
+                            TextFieldMode.Pwd -> ActionButton(actions = Actions.Pwd, isVisible = isVisible) { isVisible = !isVisible }
                             TextFieldMode.SetPwd -> Row {
                                 ActionButton(actions = Actions.Reset) { onValueChanged("") }
-                                ActionButton(actions = Actions.Pwd) { onVisibilityChanged(!isVisible) }
+                                ActionButton(actions = Actions.Pwd, isVisible = isVisible) { isVisible = !isVisible }
                             }
-                            TextFieldMode.UserName -> ActionButton(actions = Actions.Reset) { onValueChanged("") }
+                            else -> ActionButton(actions = Actions.Reset) { onValueChanged("") }
                         }
 
                     }
@@ -146,25 +139,9 @@ fun ModeBasedTextField(
         )
     }
 }
-@Composable
-fun BaseTextField(
-    modifier: Modifier = Modifier,
-    innerTextField: @Composable () -> Unit,
-    actionButtons: @Composable RowScope.() -> Unit
-){
-    Row(
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Row(modifier = Modifier.weight(1f)) {
-            innerTextField()
-        }
-        actionButtons()
-    }
-}
 
 @Composable
-fun ActionButton(actions: Actions, onClick: () -> Unit){
+fun ActionButton(actions: Actions, isVisible: Boolean = false, onClick: () -> Unit){
     when(actions){
         Actions.Reset -> IconButton(onClick = onClick) {
             Image(painter = painterResource(R.drawable.cancel_circle), contentDescription = "cancel")
