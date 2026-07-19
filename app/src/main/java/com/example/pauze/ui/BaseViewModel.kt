5ee2335798b37914a6318ce6b4b98aaf55a29e95
Daeun Lee <dev.leedaeun@gmail.com>
2026-07-19 15:25:20 +0900

    [feat]단발성 이벤트 처리 변수 생성

--- app/src/main/java/com/example/pauze/ui/BaseViewModel.kt
+++ app/src/main/java/com/example/pauze/ui/BaseViewModel.kt
@@ -3,19 +3,28 @@ package com.example.pauze.ui
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.pauze.data.model.UiState
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import java.lang.Exception
+import kotlin.contracts.ExperimentalContracts
 
-abstract class BaseViewModel: ViewModel() {
+abstract class BaseViewModel<EFFECT> : ViewModel() {
     // UI 상태 클래스를 Flow로 다룸
     private val _uiState = MutableStateFlow(UiState())
     val uiState = _uiState.asStateFlow()
+
+    // 단발성 이벤트를 처리하는 함수
+    @OptIn(ExperimentalContracts::class)
+    private val _effect = Channel<EFFECT>(Channel.BUFFERED)
+    @OptIn(ExperimentalContracts::class)
+    val effect = _effect.receiveAsFlow()
+
 
     // UI 상태를 업데이트 하는 함수
     protected fun updateState(updater: (UiState) -> UiState){
@@ -28,15 +37,23 @@ abstract class BaseViewModel: ViewModel() {
     ) {
         viewModelScope.launch {
             try {
-                updateState { it.copy(isLoading = true) }
+                updateState { it.copy(isLoading = true, error = null) }
                 block()
-            } catch (e: Exception) {
-                updateState { it.copy(error = e) }
-                println("에러 발생: ${e.message}")
+            } catch (e: CancellationException){
+                updateState { it.copy(isLoading = false, error = e) }
+                println("작업이 사용자에 의해 취소되었습니다, ${e.message}")
+            }
+            catch (e: Exception) {
+                updateState { it.copy(isLoading = false, error = e) }
+                println("예외 발생: ${e.message}")
                 e.printStackTrace()
             } finally {
-                updateState { it.copy(isLoading = false) }
+                updateState { it.copy(isLoading = false, error = null) }
             }
         }
+    }
+
+    protected fun sendEffect(effect: EFFECT){
+        _effect.trySend(effect)
     }
 }
