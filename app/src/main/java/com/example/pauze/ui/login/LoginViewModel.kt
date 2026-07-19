4470a0c4488dec006d83908bcdef05bdb45e4de5
Daeun Lee <dev.leedaeun@gmail.com>
2026-07-19 15:47:44 +0900

    [refactor]로그인 스크린 뷰모델 적용 (#22)
    * [feat]BaseViewModel 구현 (#20)
    
    * [refactor]컴포넌트 및 액티비티 이름 수정 (#22)
    
    * [refactor]로그인 스크린 뷰모델 적용 (#22)
    
    * [feat]UiState 데이터 항목 제거 (#20)
    
    * [fix]불필요한 제네릭 선언 제거 (#20)
    
    * [refactor]수정한 뷰모델에 맞게 코드 변경 (#22)
    
    * [fix]uiState 값 수정 (#20)
    
    * [feat]단발성 이벤트 처리 변수 생성
    
    * [refactor]수정한 뷰모델 반영 (#22)

---
+++ app/src/main/java/com/example/pauze/ui/login/LoginViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.pauze.ui.login
+
+import com.example.pauze.ui.BaseViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.receiveAsFlow
+
+sealed interface LoginEffect{
+    object ShowDialog: LoginEffect
+    object NavigateToHome : LoginEffect
+    object NavigateToSignUp : LoginEffect
+}
+class LoginViewModel: BaseViewModel<LoginEffect>() {
+    fun loginWithKakao(){
+        launch {
+            // todo: 카카오 로그인 구현
+            println("카카오 로그인 클릭")
+        }
+    }
+
+    fun login(email: String, pwd: String){
+        launch {
+            // todo: 로그인 로직 구현
+            println("email: $email, password: $pwd")
+            val isSuccess = false
+            if(isSuccess){
+                sendEffect(LoginEffect.NavigateToHome)
+            } else {
+                sendEffect(LoginEffect.ShowDialog)
+            }
+        }
+    }
+
+    fun toGuestMode(){
+        sendEffect(LoginEffect.NavigateToSignUp)
+    }
+}
