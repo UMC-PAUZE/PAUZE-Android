9b8d7e04b0d1c2ce600888bebb96bbf853d77e66
박한을 <keundalae67@gmail.com>
2026-07-21 19:22:52 +0900

    [fix]테스트 하느라 바꿨던 시작 화면 다시 로그인 화면으로 변경

--- app/src/main/java/com/example/pauze/MainActivity.kt
+++ app/src/main/java/com/example/pauze/MainActivity.kt
@@ -4,14 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.pauze.ui.pauze.PauzeSoundScreen
+import com.example.pauze.ui.login.LoginScreen
 import com.example.pauze.ui.theme.AppTheme
 import com.example.pauze.ui.theme.MainPaletteTheme
 import com.example.pauze.ui.theme.PAUZEAndroidTheme
@@ -23,7 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MainPaletteTheme {
-                PauzeSoundScreen()
+                LoginScreen()
             }
         }
     }
