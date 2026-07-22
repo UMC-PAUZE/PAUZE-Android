e7f7754f53936f1ccd5510553e903b321ac066c6
Daeun Lee <dev.leedaeun@gmail.com>
2026-07-22 19:33:30 +0900

    [feat]홈화면 기본 레이아웃 구현 (#57)

--- app/src/main/java/com/example/pauze/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/pauze/ui/home/HomeScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -28,6 +29,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.pauze.ui.theme.AppTheme
 import com.example.pauze.R
+import com.example.pauze.data.model.Condition
 import com.example.pauze.ui.component.Button
 import com.example.pauze.ui.component.ConditionBar
 import com.example.pauze.ui.theme.bodyTextLgBold
@@ -72,137 +74,113 @@ fun HomeScreen(
         Spacer(modifier = Modifier.height(16.dp))
         Button("오늘의 컨디션 입력하기", modifier = Modifier.fillMaxWidth(), enabled = true)
         Spacer(modifier = Modifier.height(24.dp))
-        Box (
-            modifier = Modifier
-                .fillMaxWidth()
-                .border(
-                    border = BorderStroke(
-                        width = 1.dp,
-                        color = AppTheme.palette.gray.getColor(7),
-                    ),
-                    shape = RoundedCornerShape(20.dp),
-                )
-                .background(
-                    shape = RoundedCornerShape(20.dp),
-                    color = AppTheme.palette.gray.getColor(8)
-                )
-                .padding(conditionBoxPadding.dp)
-        ){
-            Column{
-                Row{
-                    Text("어제 민감 지수", style = bodyTextLgRegular, color = AppTheme.palette.gray.getColor(2))
-                    Spacer(modifier = Modifier.width(8.dp))
-                    Text("보통", style = bodyTextLgBold, color = AppTheme.palette.tertiary.getColor(3))
-                }
-                Spacer(modifier = Modifier.height(12.dp))
-                Row(
-                    verticalAlignment = Alignment.Bottom
-                ) {
-                    Text("56", style = bodyTextXlBold.copy(fontSize = 64.sp), color = AppTheme.palette.gray.getColor(2))
-                    Spacer(modifier = Modifier.width(8.dp))
-                    Text("/ 100", style = bodyTextMdRegular.copy(fontSize = 24.sp), color = AppTheme.palette.gray.getColor(4))
-                }
-                Spacer(modifier = Modifier.height(12.dp))
-                ConditionBar(56, conditionBarWidth.dp)
-                Spacer(modifier = Modifier.height(16.dp))
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
-                    Box(
-                        modifier = Modifier
-                            .background(
-                                shape = RoundedCornerShape(16.dp),
-                                color = AppTheme.palette.gray.getColor(8)
-                            )
-                            .border(
-                                width = 1.dp,
-                                color = AppTheme.palette.gray.getColor(7),
-                                shape = RoundedCornerShape(16.dp),
-                            )
-                            .padding(12.dp)
-                            .weight(1f)
-                    ){
-                        Column{
-                            Text("수면", style = bodyTextMdMedium, color = AppTheme.palette.gray.getColor(4))
-                            Spacer(modifier = Modifier.height(12.dp))
-                            Text("부족", style = headingSmBold, color = AppTheme.palette.secondary.getColor(3))
-                        }
-                    }
-                    Box(
-                        modifier = Modifier
-                            .background(
-                                shape = RoundedCornerShape(16.dp),
-                                color = AppTheme.palette.gray.getColor(8)
-                            )
-                            .border(
-                                width = 1.dp,
-                                color = AppTheme.palette.gray.getColor(7),
-                                shape = RoundedCornerShape(16.dp),
-                            )
-                            .padding(12.dp)
-                            .weight(1f)
-                    ){
-                        Column{
-                            Text("수면", style = bodyTextMdMedium, color = AppTheme.palette.gray.getColor(4))
-                            Spacer(modifier = Modifier.height(12.dp))
-                            Text("부족", style = headingSmBold, color = AppTheme.palette.secondary.getColor(3))
-                        }
-                    }
-                    Box(
-                        modifier = Modifier
-                            .background(
-                                shape = RoundedCornerShape(16.dp),
-                                color = AppTheme.palette.gray.getColor(8)
-                            )
-                            .border(
-                                width = 1.dp,
-                                color = AppTheme.palette.gray.getColor(7),
-                                shape = RoundedCornerShape(16.dp),
-                            )
-                            .padding(12.dp)
-                            .weight(1f)
-                    ){
-                        Column{
-                            Text("수면", style = bodyTextMdMedium, color = AppTheme.palette.gray.getColor(4))
-                            Spacer(modifier = Modifier.height(12.dp))
-                            Text("부족", style = headingSmBold, color = AppTheme.palette.secondary.getColor(3))
-                        }
-                    }
-                }
+        ConditionBox(boxPadding = conditionBoxPadding, barWidth = conditionBarWidth)
+        Spacer(modifier = Modifier.height(24.dp))
+        MoveToPauzeBtn(onClick = { /*즉각 안정하기로 이동*/})
+    }
+}
+
+@Composable
+fun ConditionBox(boxPadding: Int, barWidth: Int){
+    Box (
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(
+                border = BorderStroke(
+                    width = 1.dp,
+                    color = AppTheme.palette.gray.getColor(7),
+                ),
+                shape = RoundedCornerShape(20.dp),
+            )
+            .background(
+                shape = RoundedCornerShape(20.dp),
+                color = AppTheme.palette.gray.getColor(8)
+            )
+            .padding(boxPadding.dp)
+    ){
+        Column{
+            Row{
+                Text("어제 민감 지수", style = bodyTextLgRegular, color = AppTheme.palette.gray.getColor(2))
+                Spacer(modifier = Modifier.width(8.dp))
+                Text("보통", style = bodyTextLgBold, color = AppTheme.palette.tertiary.getColor(3))
+            }
+            Spacer(modifier = Modifier.height(12.dp))
+            Row(
+                verticalAlignment = Alignment.Bottom
+            ) {
+                Text("56", style = bodyTextXlBold.copy(fontSize = 64.sp), color = AppTheme.palette.gray.getColor(2))
+                Spacer(modifier = Modifier.width(8.dp))
+                Text("/ 100", style = bodyTextMdRegular.copy(fontSize = 24.sp), color = AppTheme.palette.gray.getColor(4))
+            }
+            Spacer(modifier = Modifier.height(12.dp))
+            ConditionBar(56, barWidth.dp)
+            Spacer(modifier = Modifier.height(16.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                ConditionDetailBox(modifier = Modifier.weight(1f))
+                ConditionDetailBox(modifier = Modifier.weight(1f))
+                ConditionDetailBox(modifier = Modifier.weight(1f))
             }
         }
-        Spacer(modifier = Modifier.height(24.dp))
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(
-                    color = AppTheme.palette.tertiary.getColor(3),
-                    shape = RoundedCornerShape(20.dp)
-                )
-                .padding(16.dp),
+    }
+}
+
+@Composable
+fun ConditionDetailBox(modifier: Modifier){
+    Box(
+        modifier = Modifier
+            .background(
+                shape = RoundedCornerShape(16.dp),
+                color = AppTheme.palette.gray.getColor(8)
+            )
+            .border(
+                width = 1.dp,
+                color = AppTheme.palette.gray.getColor(7),
+                shape = RoundedCornerShape(16.dp),
+            )
+            .padding(12.dp)
+    ){
+        Column{
+            Text("수면", style = bodyTextMdMedium, color = AppTheme.palette.gray.getColor(4))
+            Spacer(modifier = Modifier.height(12.dp))
+            Text("부족", style = headingSmBold, color = AppTheme.palette.secondary.getColor(3))
+        }
+    }
+}
+
+@Composable
+fun MoveToPauzeBtn(onClick: () -> Unit){
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                color = AppTheme.palette.tertiary.getColor(3),
+                shape = RoundedCornerShape(20.dp)
+            )
+            .clickable(onClick = onClick)
+            .padding(16.dp),
+    ){
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
         ){
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center,
-            ){
-                Image(
-                    painter = painterResource(R.drawable.ic_logo),
-                    contentDescription = "앱 로고",
-                    colorFilter = ColorFilter.tint(color = AppTheme.palette.tertiary.getColor(3))
-                )
-                Spacer(modifier = Modifier.width(12.dp))
-                Column{
-                    Text("즉각 안정 시작하기", style = bodyTextXlBold, color = AppTheme.palette.tertiary.getColor(9))
-                    Text("1분 호흡으로 바로 안정해요", style = bodyTextSmRegular, color = AppTheme.palette.tertiary.getColor(9))
-                }
-                Spacer(modifier = Modifier.weight(1f))
-                Image(
-                    painter = painterResource(R.drawable.ic_arrow_forward),
-                    contentDescription = "즉각 안정 화면으로 이동",
-                    colorFilter = ColorFilter.tint(color = AppTheme.palette.tertiary.getColor(7))
-                )
+            Image(
+                painter = painterResource(R.drawable.ic_logo_yellow),
+                contentDescription = "앱 로고",
+            )
+            Spacer(modifier = Modifier.width(12.dp))
+            Column{
+                Text("즉각 안정 시작하기", style = bodyTextXlBold, color = AppTheme.palette.tertiary.getColor(9))
+                Text("1분 호흡으로 바로 안정해요", style = bodyTextSmRegular, color = AppTheme.palette.tertiary.getColor(9))
             }
+            Spacer(modifier = Modifier.weight(1f))
+            Image(
+                painter = painterResource(R.drawable.ic_arrow_forward),
+                contentDescription = "즉각 안정 화면으로 이동",
+                colorFilter = ColorFilter.tint(color = AppTheme.palette.tertiary.getColor(7))
+            )
         }
     }
 }
