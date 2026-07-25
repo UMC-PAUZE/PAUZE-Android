06d1279fb389537b6a3cf1848151c8f6fd9adc64
Daeun Lee <dev.leedaeun@gmail.com>
2026-07-25 14:55:59 +0900

    [refactor]이동 시 디테일 수정 (#53)

--- app/src/main/java/com/example/pauze/ui/login/SignUpCompletedScreen.kt
+++ app/src/main/java/com/example/pauze/ui/login/SignUpCompletedScreen.kt
@@ -40,7 +40,7 @@ fun SignUpCompletedScreen(
             when(effect){
                 SignUpCompletedEffect.NavigateToHome -> {
                     val intent = Intent(context, MainActivity::class.java).apply{
-                        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        flags = Intent.FLAG_ACTIVITY_CLEAR_TASK
                     }
                     startActivity(context, intent, null)
                 }
