99031cce32f4943186291141c128804c5046568f
Daeun Lee <dev.leedaeun@gmail.com>
2026-07-25 13:44:39 +0900

    [feat]회원가입 화면 구현 (#21)

--- app/src/main/java/com/example/pauze/ui/login/component/ModeBasedTextField.kt
+++ app/src/main/java/com/example/pauze/ui/login/component/ModeBasedTextField.kt
@@ -57,6 +57,7 @@ fun ModeBasedTextField(
         TextFieldMode.SetPwd -> false
         else -> true
     }) }
+    val nameCheck = java.util.regex.Pattern.compile("[!@#$%^&*]").matcher(value).find()
     val pwdCheck = java.util.regex.Pattern.matches("^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*]).+$", value)
 
     Column(
@@ -65,7 +66,9 @@ fun ModeBasedTextField(
                 width = 1.dp,
                 color = when {
                     value == "" -> AppTheme.palette.gray.getColor(6)
-                    (mode == TextFieldMode.UserName && value.length == 1)
+                    (mode == TextFieldMode.UserName && (value.length == 1
+                            || nameCheck
+                            || value.trim() != value))
                             || (mode == TextFieldMode.SetPwd
                             && !isFocused
                             && (value.length > 1 && value.length < 8
