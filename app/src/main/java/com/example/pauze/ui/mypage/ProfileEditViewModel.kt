99da34442df7c998bb82e6c2d1f26009fbf5bb8c
leejuyeon7852 <leejuyeon7852@gmail.com>
2026-07-28 19:03:04 +0900

    [fix]ProfileViewModel 수정 (#55)

--- app/src/main/java/com/example/pauze/ui/mypage/ProfileEditViewModel.kt
+++ app/src/main/java/com/example/pauze/ui/mypage/ProfileEditViewModel.kt
@@ -1,5 +1,8 @@
 package com.example.pauze.ui.mypage
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import com.example.pauze.data.model.BaseUiState
 import com.example.pauze.data.repository.UserProfileRepository
 import com.example.pauze.ui.BaseViewModel
@@ -11,25 +14,18 @@ sealed interface ProfileEditEffect {
 class ProfileEditViewModel : BaseViewModel<ProfileEditEffect, Unit>(
     uiState = BaseUiState(data = Unit)
 ) {
-    var nickname
-        get() = UserProfileRepository.nickname
-        set(value) { UserProfileRepository.nickname = value }
-
-    var bio
-        get() = UserProfileRepository.bio
-        set(value) { UserProfileRepository.bio = value }
-
-    var birthday
-        get() = UserProfileRepository.birthday
-        set(value) { UserProfileRepository.birthday = value }
-
-    var profileImageUri
-        get() = UserProfileRepository.profileImageUri
-        set(value) { UserProfileRepository.profileImageUri = value }
+    var nickname by mutableStateOf(UserProfileRepository.nickname)
+    var bio by mutableStateOf(UserProfileRepository.bio)
+    var birthday by mutableStateOf(UserProfileRepository.birthday)
+    var profileImageUri by mutableStateOf(UserProfileRepository.profileImageUri)
 
     fun onBackClick() = sendEffect(ProfileEditEffect.NavigateToBack)
 
     fun onSaveClick() {
+        UserProfileRepository.nickname = nickname
+        UserProfileRepository.bio = bio
+        UserProfileRepository.birthday = birthday
+        UserProfileRepository.profileImageUri = profileImageUri
         sendEffect(ProfileEditEffect.NavigateToBack)
     }
 }
