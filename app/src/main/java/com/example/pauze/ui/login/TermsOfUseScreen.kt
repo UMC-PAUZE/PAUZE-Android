ecf6a96344a0c638326871d28cb6b54b25d4ba53
Daeun Lee <dev.leedaeun@gmail.com>
2026-07-30 15:31:41 +0900

    [feat]이용 약관 및 개인정보 처리 화면 (#92)

--- app/src/main/java/com/example/pauze/ui/login/TermsOfUseScreen.kt
+++ app/src/main/java/com/example/pauze/ui/login/TermsOfUseScreen.kt
@@ -19,11 +19,11 @@ import com.example.pauze.ui.theme.bodyTextXlBold
 
 
 @Composable
-fun PrivacyPolicyScreen(
+fun TermsOfUseScreen(
     modifier : Modifier = Modifier,
     viewModel: TermsAndPolicyViewModel = viewModel()
 ){
-    val policies = viewModel.privacyPolicies
+    val terms = viewModel.termsOfUse
 
     LazyColumn(
         modifier = modifier
@@ -33,26 +33,28 @@ fun PrivacyPolicyScreen(
     ){
         item {
             Text(
-                "PAUZE 개인정보 처리방침",
+                "PAUZE 서비스 이용약관",
                 style = bodyTextXlBold,
                 color = AppTheme.palette.gray.getColor(2)
             )
             Spacer(modifier = Modifier.height(24.dp))
             Text(
-                "PAUZE는 이용자의 개인정보를 소중히 여깁니다.\n수집한 정보는 서비스 제공 및 예민함 분석 목적으로만 사용되며, 제3자에게 제공되지 않습니다",
+                "이 약관은 PAUZE 서비스 이용에 관한 기본 규칙을 정합니다.\n" +
+                        "\n" +
+                        "회원가입 또는 서비스 시작 시 본 약관에 동의한 것으로 간주됩니다.",
                 style = bodyTextMdRegular,
                 color = AppTheme.palette.gray.getColor(4)
             )
         }
-        items(policies.size){ index ->
-            if(policies[index].second == ""){
+        items(terms.size){ index ->
+            if(terms[index].second == ""){
                 Spacer(modifier = Modifier.height(48.dp))
-                Text(policies[index].first, style = bodyTextXlBold, color = AppTheme.palette.gray.getColor(2))
+                Text(terms[index].first, style = bodyTextXlBold, color = AppTheme.palette.gray.getColor(2))
             } else {
                 Spacer(modifier = Modifier.height(24.dp))
                 TermAndPolicyText(
-                    policies[index].first,
-                    policies[index].second
+                    terms[index].first,
+                    terms[index].second
                 )
             }
         }
