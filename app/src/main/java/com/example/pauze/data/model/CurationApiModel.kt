37bc41be25c45cc6878e06798882fbaf703d64b0
Shin Han Seo <imkara1@naver.com>
2026-08-09 13:36:54 +0900

    [feat] 내가 누른 좋아요 목록 모델 추가(#138)

--- app/src/main/java/com/example/pauze/data/model/CurationApiModel.kt
+++ app/src/main/java/com/example/pauze/data/model/CurationApiModel.kt
@@ -74,6 +74,27 @@ data class MyBookmarkListItemDto(
     val createdAt: String,
 )
 
+data class MyLikeListResultDto(
+    val content: List<MyLikeListItemDto>,
+    val page: Int,
+    val size: Int,
+    val totalElements: Int,
+    val totalPages: Int,
+)
+
+data class MyLikeListItemDto(
+    val likesId: Long,
+    val postId: Long,
+    val categoryId: Long,
+    val categoryName: String,
+    val title: String,
+    val estimatedReadTime: Int,
+    val summary: String,
+    val likeCount: Int,
+    val isBookmarked: Boolean,
+    val createdAt: String,
+)
+
 fun CurationPostListItemDto.toCurationPost(): CurationPost {
     return CurationPost(
         postId = postId,
