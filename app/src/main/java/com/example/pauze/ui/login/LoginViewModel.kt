469ce54a054c490909eca37f7d5ec59e17129154
박한을 <keundalae67@gmail.com>
2026-08-13 22:10:40 +0900

    [fix]임시 로그인 삭제

--- app/src/main/java/com/example/pauze/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/example/pauze/ui/login/LoginViewModel.kt
@@ -1,13 +1,7 @@
 package com.example.pauze.ui.login
 
 import com.example.pauze.data.model.BaseUiState
-import com.example.pauze.data.model.LocalLoginRequestDto
-import com.example.pauze.data.model.getOrThrow
-import com.example.pauze.data.repository.TokenRepository
-import com.example.pauze.data.service.AuthService
 import com.example.pauze.ui.BaseViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
 
 sealed interface LoginEffect{
     object ShowDialog: LoginEffect
@@ -15,53 +9,30 @@ sealed interface LoginEffect{
     object NavigateToAdditionalScreen: LoginEffect
     object NavigateToSignUp : LoginEffect
 }
-@HiltViewModel
-class LoginViewModel @Inject constructor(
-    private val authService: AuthService
-): BaseViewModel<LoginEffect, Boolean>(
+class LoginViewModel(): BaseViewModel<LoginEffect, Boolean>(
     uiState = BaseUiState(data = false)
 ) {
-    fun loginWithKakao(){
-        launch(
-            onSuccess = { result: Boolean ->
-                updateData { result }
-                sendEffect(LoginEffect.NavigateToAdditionalScreen)
-            },
-            onFailure = {
-                updateData { false }
-                sendEffect(LoginEffect.ShowDialog)
-            }
-        ) {
+        fun loginWithKakao(){
+        launch {
             // todo: 카카오 로그인 구현
-            true
+            val result = true
+            if (result) sendEffect(LoginEffect.NavigateToAdditionalScreen)
+            else sendEffect(LoginEffect.ShowDialog)
+            result
         }
     }
 
     fun login(email: String, pwd: String){
-        TokenRepository.accessToken = null
-        launch(
-            onSuccess = { accessToken: String ->
-                TokenRepository.accessToken = accessToken
-                updateData { true }
-                sendEffect(LoginEffect.NavigateToHome)
-            },
-            onFailure = {
-                TokenRepository.accessToken = null
-                updateData { false }
-                sendEffect(LoginEffect.ShowDialog)
-            }
-        ) {
-            authService.login(
-                LocalLoginRequestDto(
-                    email = email.trim(),
-                    password = pwd
-                )
-            ).getOrThrow().accessToken
+        launch {
+            // todo: 로그인 로직 구현
+            val result = false
+            if (result) sendEffect(LoginEffect.NavigateToHome)
+            else sendEffect(LoginEffect.ShowDialog)
+            result
         }
     }
 
     fun toGuestMode(){
-        TokenRepository.accessToken = null
         sendEffect(LoginEffect.NavigateToHome)
     }
     fun toSignUp(){
