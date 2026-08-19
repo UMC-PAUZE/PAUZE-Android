133a28f90515cac9844d2ec3e67db161bfb970c1
Daeun Lee <dev.leedaeun@gmail.com>
2026-08-19 19:29:01 +0900

    [refactor]인증 코드 필드와 성공/실패 문자 간격 띄우기 (#20$)

--- app/src/main/java/com/example/pauze/ui/login/component/EnterVerificationCode.kt
+++ app/src/main/java/com/example/pauze/ui/login/component/EnterVerificationCode.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
@@ -65,6 +66,7 @@ fun EnterVerificationCode(
         if(!isFocused
             && (isLinkingScreen && (viewModel as LinkingViewModel).code != "" && viewModel.isVerified != null)
             || (!isLinkingScreen && (viewModel as SignUpViewModel).code != "" && viewModel.isVerified != null)){
+            Spacer(modifier = Modifier.height(4.dp))
             Text(
                 if((isLinkingScreen && (viewModel as LinkingViewModel).isVerified == true)
                     || (!isLinkingScreen && (viewModel as SignUpViewModel).isVerified == true)) "인증에 성공했습니다"
